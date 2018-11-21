8b15a5c5cb7fb58e5e8d160b0bdd17284c45455d
sethuram ramalinga reddy <sethuram500@gmail.com>
2018-11-21 13:27:02 +0000

    added function to respond signin request

--- app.py
+++ app.py
@@ -7,6 +7,22 @@ import mongo_connection as mc
 app = Flask(__name__)
 
 
+def data_extraction(data):
+    extracted_data = str(data).replace("b","",1).replace("'","").strip()
+    login_credentials = {
+         "username": "",
+         "password": ""
+     }
+
+    if (extracted_data.split(",")[0].split(":")[0] == "user_name"):
+         login_credentials["username"] = extracted_data.split(",")[0].split(":")[1]
+    if (extracted_data.split(",")[1].split(":")[0] == "password"):
+         login_credentials["password"] = extracted_data.split(",")[1].split(":")[1]
+    return login_credentials
+         
+
+
+
 @app.route('/login',methods=['POST'])
 def insert():
      received_data = str(request.data).replace("b","",1).replace("'","").strip()
@@ -21,12 +37,7 @@ def insert():
      if (received_data.split(",")[1].split(":")[0] == "password"):
          login_credentials["password"] = received_data.split(",")[1].split(":")[1]
 
-
-
      result = mc.insert(login_credentials)
-
-
-
 
      if (result) != "":
          print("The User data is inserted successfully.......")
@@ -35,7 +46,20 @@ def insert():
          print("The data insertion resulted in failure.......")
          return jsonify({"status":False})
 
-     return
+     return # have to check why this exists ?
+   
+@app.route('/signin', method = "POST")
+def find_one():
+    login_credentials = data_extraction(request.data)
+    result = mc.find_one(login_credentials)
+    if (result) != "":
+         print("The User data is exists .......")
+         return  jsonify({"status":True})
+     else:
+         print("The data doesn't exists.......")
+         return jsonify({"status":False})
+    
+
 
 if __name__ == '__main__':
     app.run()
