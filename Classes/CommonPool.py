a99d5f27f4406d576f4553818055e5b1922178da
Sethuram Ramalinga Reddy <ramalins@tcd.ie>
2019-05-17 19:38:17 +0100

    cleaned up

--- Classes/CommonPool.py
+++
@@ -1,6 +0,0 @@
-class CommonPool():
-    def acquire(self):
-        return self._reusables.pop()
-
-    def release(self, reusable):
-        self._reusables.append(reusable)
--- config.py
+++
@@ -1,2 +0,0 @@
-SQLALCHEMY_DATABASE_URI = 'postgresql://postgres:Luffy@143@localhost/smart_share'
-SQLALCHEMY_TRACK_MODIFICATIONS = False
